12f17a85f79fed327c8a647e7385c8ccfa988621
Albert <albertsadykovv@gmail.com>
2025-11-03 15:28:06 +0600

    вёрстка pc версии

--- script.js
+++ script.js
@@ -20,3 +20,73 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const slider = new Swiper('.quotes__carousel', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: false,
+    navigation: {
+      nextEl: '.quotes__nav .swiper-button-next',
+      prevEl: '.quotes__nav .swiper-button-prev'
+    },
+    speed: 600,
+    grabCursor: true,
+    breakpoints: {
+      320: { slidesPerView: 1, spaceBetween: 12 },
+      700: { slidesPerView: 1, spaceBetween: 16 },
+      1024: { slidesPerView: 2, spaceBetween: 20 }
+    }
+  });
+
+  document.querySelectorAll('.card-toggle').forEach(btn => {
+    btn.addEventListener('click', function (e) {
+      const card = btn.closest('.quotes__card');
+      const expanded = card.classList.toggle('is-open');
+      btn.setAttribute('aria-expanded', expanded ? 'true' : 'false');
+    });
+  });
+
+
+const teamSwiper = new Swiper('.team__carousel', {
+  slidesPerView: 6, // для самых больших экранов
+  spaceBetween: 20,
+  loop: false,
+  navigation: {
+    nextEl: '.team__nav .swiper-button-next',
+    prevEl: '.team__nav .swiper-button-prev'
+  },
+  speed: 600,
+  grabCursor: true,
+  breakpoints: {
+    1600: { slidesPerView: 6 },  // экраны 1600px+
+    1200: { slidesPerView: 6 },
+    880: { slidesPerView: 5 },
+    600: { slidesPerView: 3 },
+    0: { slidesPerView: 2},
+  },
+});
+
+
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll('.quotes__card');
+
+  cards.forEach(card => {
+    const toggle = card.querySelector('.card-toggle');
+
+    toggle.addEventListener('click', () => {
+      card.classList.toggle('active');
+
+  
+      toggle.textContent = card.classList.contains('active') ? '-' : '+';
+    });
+  });
+});
+
+
